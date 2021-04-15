91155bc43f548d28e5dffdf9f6554fdb9f9f1c71
Justin Bishop <jubishop@gmail.com>
2021-04-14 21:17:38 -0700

    support google_fonts tag in here

--- lib/sinatra/static.rb
+++ lib/sinatra/static.rb
@@ -24,6 +24,15 @@ module Sinatra
                   media="#{media}" />)
         }.join("\n")
       end
+
+      def google_fonts(*fonts)
+        families = fonts.map { |font| "family=#{font}" }.join('&')
+        source = "https://fonts.googleapis.com/css2?#{families}&display=swap"
+        <<~HTML
+          #{preconnect_link_tag('https://fonts.gstatic.com')}
+          #{stylesheet_link_tag(source)}
+        HTML
+      end
     end
 
     module Private
